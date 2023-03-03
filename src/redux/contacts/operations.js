008d88f6e1a401d62162f5c94b7e56e41241fd69
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-03 22:10:38 +0200

    in work

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,9 +5,11 @@ export const getContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkApi) => {
     try {
+      // const token = localStorage.getItem('token');
       const data = await contactsAPI.fetchContacts();
       return data;
     } catch (error) {
+      console.log(error.message);
       return thunkApi.rejectWithValue(error.message);
     }
   }
@@ -17,10 +19,12 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkApi) => {
     try {
+      // const token = localStorage.getItem('token');
       const data = await contactsAPI.addContact(contact);
       console.log('add', data);
       return data;
     } catch (error) {
+      console.log(error.message);
       return thunkApi.rejectWithValue(error.message);
     }
   }
@@ -30,10 +34,11 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (id, thunkApi) => {
     try {
+      // const token = localStorage.getItem('token');
       const data = await contactsAPI.deleteContact(id);
       return data;
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
       return thunkApi.rejectWithValue(error.message);
     }
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://63fcff2a8ef914c555a3c3cc.mockapi.io';
-//
-//
-
 const $publicHost = axios.create({
   baseURL: 'https://connections-api.herokuapp.com',
   headers: { 'Content-Type': 'application/json' },
@@ -14,7 +10,6 @@ const $privateHost = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
-//c: add token save to localStorage
 const authInterceptor = config => {
   config.headers['Authorization'] = localStorage.getItem('token');
   return config;
@@ -44,15 +39,15 @@ export const userAPI = {
 
 export const contactsAPI = {
   async fetchContacts() {
-    const { data } = await axios.get('/contacts');
+    const { data } = await $privateHost.get('/contacts');
     return data;
   },
   async addContact(contact) {
-    const { data } = await axios.post('/contacts', contact);
+    const { data } = await $privateHost.post('/contacts', contact);
     return data;
   },
   async deleteContact(id) {
-    const { data } = await axios.delete(`/contacts/${id}`, id);
+    const { data } = await $privateHost.delete(`/contacts/${id}`, id);
     return data;
   },
 };
